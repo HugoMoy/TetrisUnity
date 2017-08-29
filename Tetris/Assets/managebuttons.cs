671ea9b702e772f19c19465e8d256005b9be8b4c
Hugo Moynac <hugo.moynac@gmail.com>
2017-08-29 19:58:23 -0300

    Added StartScene and ugly images (for now)

---
+++ Tetris/Assets/managebuttons.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class managebuttons : MonoBehaviour {
+
+	public void NewGameButton(string NewGameLevel) {
+			SceneManager.LoadScene(NewGameLevel);
+		
+	}
+	public void ExitGameButton(){
+		Application.Quit();
+	} 
+}
